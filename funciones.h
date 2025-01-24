1468a5947cdde860438802c651f17b65446b3a84
PedroVargas812 <pedro.vargas@udla.edu.ec>
2025-01-24 07:26:07 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,5 +1,12 @@
+// funciones.h
+
 #define NUM_ZONAS 5
 #define MAX_NOMBRE 50
+#define LIMITE_PM25 25.0
+#define LIMITE_NO2 40.0
+#define LIMITE_SO2 20.0
+#define LIMITE_O3 100.0
+#define LIMITE_CO 10.0
 
 typedef struct {
     char nombre[MAX_NOMBRE];
@@ -19,5 +26,8 @@ void guardarZonasEnArchivo(Zona *zonas, const char *archivo);
 void leerDatosDesdeArchivo(Zona *zonas, const char *archivo);
 void mostrarDatos(Zona *zona);
 void editarParametros(Zona *zona);
+void calcularPromediosHistoricos(Zona *zonas, const char *archivo);
+void generarRecomendaciones(Zona *zona);
+void prediccionContaminacion(Zona *zona);
 int archivoExiste(const char *archivo);
-
+void emitirAlertas(Zona *zonas);
